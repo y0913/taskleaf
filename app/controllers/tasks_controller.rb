eafeafa14d23fafb6e0a4a3abf1ea5c5c5d301bd
y0913 <yohei19910825@gmail.com>
2019-01-18 00:22:43 +0900

    Add some changes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,9 +12,13 @@ class TasksController < ApplicationController
   end
 
   def create
-  	task = Task.new(task_params)
-  	task.save!
-  	redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました"
+  	@task = Task.new(task_params)
+
+  	if @task.save
+  	 redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました"
+    else
+     render :new
+    end
   end
 
   def edit
@@ -25,6 +29,12 @@ class TasksController < ApplicationController
   	task = Task.find(params[:id])
   	task.update!(task_params)
   	redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_073536) do
+ActiveRecord::Schema.define(version: 2019_01_17_125752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name", unique: true
   end
 
 end
